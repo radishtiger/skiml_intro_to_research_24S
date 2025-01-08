2e42852a818525c940b1f1f691d723b454c3325f
radishtiger <anmh9161@naver.com>
2025-01-08 17:03:11 +0900

    update_for_2425Winter

--- run.sh
+++ run.sh
@@ -3,11 +3,12 @@
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --time=0-12:00:00
-#SBATCH --output=/home/moohosong/data/skiml_intro_to_research_24S/logs/%j.out         # Adjustement required: Write directory you want
-#SBATCH --error=/home/moohosong/data/skiml_intro_to_research_24S/logs/%j.err          # Adjustement required: Write directory you want
-#SBATCH --mem=64000MB
+#SBATCH --output=logs/%j.out      
+#SBATCH --error=logs/%j.err       
+#SBATCH --mem=16000MB
 #SBATCH --cpus-per-task=2
-source /home/${USER}/data/.bashrc
+source ~/data/.bashrc
 source ~/data/miniconda3/etc/profile.d/conda.sh
 
+conda activate wandb-tutorial
 srun python wandb_tutorial_sbatch.py
